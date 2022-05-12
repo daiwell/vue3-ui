d7788a93cbec093c62b003169f296f5599d4d303
daiwell <9782309+daiwell77@user.noreply.gitee.com>
2022-05-12 15:23:06 +0800

    初始化vue-router并解决了ts文件引入vue文件报错

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<router-link to="/home">首页</router-link>
+<router-link to="/about">首页</router-link>
+<router-view></router-view>
 </template>
 
 <style>
